d74e57bf1d8dae9a703bd712048e00ce904e9560
Barbora6 <smetanovabarbora@email.cz>
2025-05-21 13:55:19 +0200

    api github

--- react-projects-1/src/components/github-profil/GithubProfileFinder.jsx
+++ react-projects-1/src/components/github-profil/GithubProfileFinder.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import "./Style.css";
 
 export const GithubProfileFinder = () => {
+  const [userName, setUserName] = useState("Barbora6");
+
+  const fetchGithubUserData = async () => {
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const handleSubmit = () => {};
+
+  useEffect(() => {
+    fetchGithubUserData();
+  });
+
   return (
-    <div>
-      <h1>Github</h1>
+    <div className="github-profile-container">
+      <div className="input-wrapper">
+        <input
+          type="text"
+          name="search-by-username"
+          placeholder="Search Github Username..."
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button onClick={handleSubmit}>Search</button>
+      </div>
     </div>
   );
 };
