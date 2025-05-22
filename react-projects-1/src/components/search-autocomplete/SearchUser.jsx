8f38147bb4a50db1bcc376a857769959790815da
Barbora6 <smetanovabarbora@email.cz>
2025-05-22 20:47:38 +0200

    filtrování uživatelů

--- react-projects-1/src/components/search-autocomplete/SearchUser.jsx
+++ react-projects-1/src/components/search-autocomplete/SearchUser.jsx
@@ -4,13 +4,37 @@ export const SearchUser = () => {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
   const [error, setError] = useState(null);
+  const [searchParam, setSearchParam] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  const handleChange = (event) => {
+    const query = event.target.value.toLowerCase();
+    setSearchParam(query);
+    if (query.length > 1) {
+      const filteredData =
+        users && users.length
+          ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+          : [];
+      setFilteredUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  };
 
   const fetchListOfUsers = async () => {
     try {
+      setLoading(true);
       const response = await fetch("https://dummyjson.com/users");
       const data = await response.json();
-      console.log(data);
+      if (data && data.users && data.users.length) {
+        setUsers(data.users.map((userItem) => userItem.firstName));
+        setLoading(false);
+        setError(null);
+      }
     } catch (error) {
+      setLoading(false);
       console.log(error);
       setError(error);
     }
@@ -20,12 +44,16 @@ export const SearchUser = () => {
     fetchListOfUsers();
   }, []);
 
+  console.log(users, filteredUsers);
+
   return (
     <div className="search-autocomplete-container">
       <input
         type="text"
         name="search-users"
         placeholder="Vyhledej uživatele..."
+        value={searchParam}
+        onChange={handleChange}
       />
     </div>
   );
