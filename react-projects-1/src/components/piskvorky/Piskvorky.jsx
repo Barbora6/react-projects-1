ec362eff91d7612a02408f93a1a058cc30f5aaeb
Barbora6 <smetanovabarbora@email.cz>
2025-05-23 20:46:35 +0200

    handleClick

--- react-projects-1/src/components/piskvorky/Piskvorky.jsx
+++ react-projects-1/src/components/piskvorky/Piskvorky.jsx
@@ -22,24 +22,29 @@ export const Piskvorky = () => {
   const [ctverce, setCtverce] = useState(Array(9).fill(""));
   const [kdoTahne, setKdoTahne] = useState(true);
 
-  console.log(ctverce);
+  const handleClick = (aktualniCtverec) => {
+    let vybraneCtverce = [...ctverce];
+    vybraneCtverce[aktualniCtverec] = kdoTahne ? "X" : "O";
+    setKdoTahne(!kdoTahne);
+    setCtverce(vybraneCtverce);
+  };
 
   return (
     <div className="tic-tac-toe-container">
       <div className="row">
-        <Ctverec />
-        <Ctverec />
-        <Ctverec />
+        <Ctverec onClick={() => handleClick(0)} />
+        <Ctverec onClick={() => handleClick(1)} />
+        <Ctverec onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Ctverec />
-        <Ctverec />
-        <Ctverec />
+        <Ctverec onClick={() => handleClick(3)} />
+        <Ctverec onClick={() => handleClick(4)} />
+        <Ctverec onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Ctverec />
-        <Ctverec />
-        <Ctverec />
+        <Ctverec onClick={() => handleClick(6)} />
+        <Ctverec onClick={() => handleClick(7)} />
+        <Ctverec onClick={() => handleClick(8)} />
       </div>
     </div>
   );
