0a858d263ea4cb3ec207355f474456b01629c37a
Barbora6 <smetanovabarbora@email.cz>
2025-05-23 21:49:06 +0200

    zjisti vítěze

--- react-projects-1/src/components/piskvorky/Piskvorky.jsx
+++ react-projects-1/src/components/piskvorky/Piskvorky.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Piskvorky.css";
 
 // indexy políček v mřížce budou rozloženy
@@ -21,30 +21,66 @@ const Ctverec = ({ value, onClick }) => {
 export const Piskvorky = () => {
   const [ctverce, setCtverce] = useState(Array(9).fill(""));
   const [kdoTahne, setKdoTahne] = useState(true);
+  const [status, setStatus] = useState("");
+
+  const zjistiViteze = (ctverce) => {
+    const vitezneVzory = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < zjistiViteze.length; i++) {
+      const [x, y, z] = vitezneVzory[i];
+
+      if (
+        ctverce[x] &&
+        ctverce[x] === ctverce[y] &&
+        ctverce[x] === ctverce[z]
+      ) {
+        return ctverce[x];
+      }
+    }
+    return null;
+  };
 
   const handleClick = (aktualniCtverec) => {
     let vybraneCtverce = [...ctverce];
+    if (zjistiViteze(vybraneCtverce) || vybraneCtverce[aktualniCtverec]) return;
     vybraneCtverce[aktualniCtverec] = kdoTahne ? "X" : "O";
     setKdoTahne(!kdoTahne);
     setCtverce(vybraneCtverce);
   };
 
+  useEffect(() => {
+    if (!zjistiViteze(ctverce) && ctverce.every((item) => item !== "")) {
+      setCtverce(`Prohráli jste. Prosím resetujte hru!`);
+    } else if (zjistiViteze(ctverce)) {
+      setStatus(`Vítězem je ${zjistiViteze(ctverce)}`);
+    }
+  }, [ctverce, kdoTahne]);
+
   return (
     <div className="tic-tac-toe-container">
       <div className="row">
-        <Ctverec onClick={() => handleClick(0)} />
-        <Ctverec onClick={() => handleClick(1)} />
-        <Ctverec onClick={() => handleClick(2)} />
+        <Ctverec value={ctverce[0]} onClick={() => handleClick(0)} />
+        <Ctverec value={ctverce[1]} onClick={() => handleClick(1)} />
+        <Ctverec value={ctverce[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="row">
-        <Ctverec onClick={() => handleClick(3)} />
-        <Ctverec onClick={() => handleClick(4)} />
-        <Ctverec onClick={() => handleClick(5)} />
+        <Ctverec value={ctverce[3]} onClick={() => handleClick(3)} />
+        <Ctverec value={ctverce[4]} onClick={() => handleClick(4)} />
+        <Ctverec value={ctverce[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="row">
-        <Ctverec onClick={() => handleClick(6)} />
-        <Ctverec onClick={() => handleClick(7)} />
-        <Ctverec onClick={() => handleClick(8)} />
+        <Ctverec value={ctverce[6]} onClick={() => handleClick(6)} />
+        <Ctverec value={ctverce[7]} onClick={() => handleClick(7)} />
+        <Ctverec value={ctverce[8]} onClick={() => handleClick(8)} />
       </div>
     </div>
   );
