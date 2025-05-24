19bbfe8756eb8c758ebac6f143a0ee3fe60482f9
Barbora6 <smetanovabarbora@email.cz>
2025-05-24 22:48:06 +0200

    loading

--- react-projects-1/src/components/weather-app/WeatherApp.jsx
+++ react-projects-1/src/components/weather-app/WeatherApp.jsx
@@ -3,14 +3,34 @@ import { Search } from "./Search";
 
 export const WeatherApp = () => {
   const [search, setSearch] = useState("");
-  const[loading, setLoading] = useState(false);
-  const[weatherData, setWeatherData] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
 
-  const handleSearch = async () => {
-const responce = 
+  const fetchWeatherData = async (param) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q={param}&appid=927d9593b1e91050f32ce5347dd25b3b`
+      );
+      const data = await response.json();
+      console.log(data, "data");
 
-
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
+    }
   };
+
+  const handleSearch = () => {
+    fetchWeatherData(search);
+  };
+
+  console.log(loading);
+
   return (
     <div>
       <Search
